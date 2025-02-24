b0205b7d6cc78a0a1748326b40a28dd72ddd2387
Aditya Maurya <mauryaaditya7405@gmail.com>
2025-02-25 00:56:39 +0530

    Add Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
+apply(plugin = "kotlin-kapt")
 
 android {
     namespace = "com.adityacode.grownest"
@@ -36,6 +38,20 @@ android {
 }
 
 dependencies {
+    // Room Database
+    implementation ("androidx.room:room-runtime:2.6.1")
+    kapt ("androidx.room:room-compiler:2.6.1")
+    implementation ("androidx.room:room-ktx:2.6.1")
+
+    // WorkManager for reminders
+    implementation ("androidx.work:work-runtime-ktx:2.9.0")
+
+    // Lifecycle and ViewModel
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+
+    // Picasso for image loading
+    implementation ("com.squareup.picasso:picasso:2.71828")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
